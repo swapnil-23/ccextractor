4d8d0133fcbdb610c4f6815df43a27d52a470b34
swapnil-23 <swappnilbiswas2013@gmail.com>
2024-03-24 23:18:44 +0530

    made connection to mysql

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import json
+import random
 import string
 from flask import Flask, render_template, url_for, redirect, request
-import random
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import yaml
+import re
 
 app = Flask(__name__, template_folder="templates")
 
-shorten_urls = {}
+
+## taking the config from MySQL yaml 
+with open('db.yaml', 'r') as file:
+    db = yaml.safe_load(file)
+
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
 
 
 ## making the logic for the url shortner
@@ -16,7 +31,8 @@ def url_shortener(length = 6):
 
 
 ## adding to the main homepage
-@app.route('/', methods = ["GET", "POST"])
+## adding to the main homepage
+@app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         long_url = request.form['long_url']
@@ -24,26 +40,30 @@ def home():
         while short_url in shorten_urls:
             short_url = url_shortener()
         
-        shorten_urls[short_url] = long_url
-        with open("url.json", "w") as f:
-            print ("TEST")
-            json.dump(shorten_urls, f)
-        return f"Shortend URLS: {request.url_root}{short_url}"
+        # Storing the URL in MySQL database
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO urls (short_url, long_url) VALUES (%s, %s)", (short_url, long_url))
+        mysql.connection.commit()
+        cur.close()
+        
+        return f"Shortened URL: {request.url_root}{short_url}"
     return render_template("index.html")
-
+        
 
 ## making the redirect to the shortend url
 @app.route("/<short_url>")
 def redirect_url(short_url):
-    long_url = shorten_urls.get(short_url)
-    if long_url:
-        return redirect(long_url)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT long_url FROM urls WHERE short_url = %s", [short_url])
+    url = cur.fetchone()
+    cur.close()
+    
+    if url:
+        return redirect(url[0])
     else:
         return "URL not found ERROR 404"
 
 
 if (__name__) == "__main__":
-    with open ("urls.json", "r") as f:
-        shorten_urls = json.load(f)
     app.run(debug=True)
 
